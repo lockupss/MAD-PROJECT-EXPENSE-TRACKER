71dee965bf226885754d7c5eaab5b110e1188152
Abinet Argaw <abinet122ar@gmail.com>
2026-01-19 02:41:47 -0500

    empty ExpenseRepository class
    Removed the ExpenseRepository class and its associated imports.

--- app/src/main/java/com/example/expense/ExpenseRepository.java
+++ app/src/main/java/com/example/expense/ExpenseRepository.java
@@ -1,8 +1,1 @@
-package com.example.expense;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class ExpenseRepository {
-    public static List<Expense> expenses = new ArrayList<>();
-}
